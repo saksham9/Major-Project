8b14ba79a8a8cf05de8e38ea527f8152f99fbbaf
saksham <agrawal.saksham9@gmail.com>
2021-04-16 23:28:54 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
 import React from 'react';
-import logo from './logo.svg';
+import {Route,Switch} from 'react-router-dom';
+import Home from './components/Home/Home';
+import Signin from './components/SignIn/SignIn';
+import Signup from './components/Signup/Signup';
+import Dashboard from "./ClientDashboard/Dashboard";
+import Userhome from "./ClientDashboard/pages/Userhome";
+import Cart from "./ClientDashboard/pages/cart";
+import Allprices from './ClientDashboard/pages/all_prices';
+import Weather from './ClientDashboard/pages/weather';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <Switch> 
+      <Route path="/login" exact component={Signin}/>
+      <Route path="/signup" exact component={Signup}/>  
+      <Route path="/dashboard" exact component={Dashboard}/>
+      <Route path="/" exact component={Home}/>
+    </Switch>
     </div>
   );
 }
